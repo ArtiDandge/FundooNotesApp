26012dcddceeb32aae49a11a765f33bd9c11b2e6
ArtiDandge <58045752+ArtiDandge@users.noreply.github.com>
2021-01-26 14:44:29 +0530

    [Arti Dandge] Add: StyleCop Rule For UserManager.cs and IUserManager.cs, make sure no rule voilation is there

--- FundooManager/Interfaces/IUserManager.cs
+++ FundooManager/Interfaces/IUserManager.cs
@@ -1,13 +1,35 @@
-﻿using FundooModels;
-using System;
-using System.Collections.Generic;
-using System.Text;
+﻿// --------------------------------------------------------------------------------------------------------------------
+// <copyright file="IUserManager.cs" company="Bridgelabz">
+//   Copyright © 2021 Company="BridgeLabz"
+// </copyright>
+// <creator name="Dandge Arti Subhash"/>
+// --------------------------------------------------------------------------------------------------------------------
 
 namespace FundooManager.Interfaces
 {
+    using System;
+    using System.Collections.Generic;
+    using System.Text;
+    using FundooModels;
+
+    /// <summary>
+    /// IUserManager Interface
+    /// </summary>
     public interface IUserManager
     {
+        /// <summary>
+        /// AddNewUser Method Declaration
+        /// </summary>
+        /// <param name="user">user parameter for this method</param>
+        /// <returns>string message</returns>
         public string AddNewUser(RegistrationModel user);
+
+        /// <summary>
+        /// Login Method Declaration  
+        /// </summary>
+        /// <param name="email">user email parameter</param>
+        /// <param name="password">user password parameter</param>
+        /// <returns>string message</returns>
         public string Login(string email, string password);
     }
 }
--- FundooManager/Manager/UserManager.cs
+++ FundooManager/Manager/UserManager.cs
@@ -1,27 +1,55 @@
-﻿using FundooManager.Interfaces;
-using FundooModels;
-using FundooRepository.Interfaces;
-using System;
-using System.Collections.Generic;
-using System.Text;
+﻿// --------------------------------------------------------------------------------------------------------------------
+// <copyright file="UserManager.cs" company="Bridgelabz">
+//   Copyright © 2021 Company="BridgeLabz"
+// </copyright>
+// <creator name="Dandge Arti Subhash"/>
+// --------------------------------------------------------------------------------------------------------------------
 
 namespace FundooManager.Manager
 {
+    using System;
+    using System.Collections.Generic;
+    using System.Text;
+    using FundooManager.Interfaces;
+    using FundooModels;
+    using FundooRepository.Interfaces;
+
+    /// <summary>
+    /// UserManager class implements IUserManager interface
+    /// </summary>
     public class UserManager : IUserManager
     {
+        /// <summary>
+        /// userRegistration reference variable of type IUserRegistration
+        /// </summary>
         private readonly IUserRegistration userRegistration;
-        
+
+        /// <summary>
+        /// Initializes a new instance of the <see cref="UserManager" /> class.
+        /// </summary>
+        /// <param name="userRegistration">userRegistration parameter</param>
         public UserManager(IUserRegistration userRegistration)
         {
             this.userRegistration = userRegistration;
         }
 
+        /// <summary>
+        /// Method to call AddNewUser Method which belongs to IUserRegistration interface
+        /// </summary>
+        /// <param name="user">user parameter</param>
+        /// <returns>string message</returns>
         public string AddNewUser(RegistrationModel user)
         {
             string message = this.userRegistration.AddNewUser(user);
             return message;
         }
 
+        /// <summary>
+        /// Method to call Login Method which belongs to IUserRegistration interface
+        /// </summary>
+        /// <param name="email">user email</param>
+        /// <param name="password">user password</param>
+        /// <returns>string message</returns>
         public string Login(string email, string password)
         {
             string message = this.userRegistration.Login(email, password);
