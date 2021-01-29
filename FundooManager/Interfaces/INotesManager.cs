5e1554af80689997fea6784f2745810b431cd66a
ArtiDandge <58045752+ArtiDandge@users.noreply.github.com>
2021-01-29 10:05:48 +0530

    [Arti Dandge] Refactor: Added StyleCop to make sure that there is no rule voilation in INotesManager

--- FundooManager/Interfaces/INotesManager.cs
+++ FundooManager/Interfaces/INotesManager.cs
@@ -1,15 +1,47 @@
-﻿using FundooModels;
-using System;
-using System.Collections.Generic;
-using System.Text;
+﻿// --------------------------------------------------------------------------------------------------------------------
+// <copyright file="INotesManager.cs" company="Bridgelabz">
+//   Copyright © 2021 Company="BridgeLabz"
+// </copyright>
+// <creator name="Dandge Arti Subhash"/>
+// --------------------------------------------------------------------------------------------------------------------
 
 namespace FundooManager.Interfaces
 {
+    using System;
+    using System.Collections.Generic;
+    using System.Text;
+    using FundooModels;
+
+    /// <summary>
+    /// INotesManager interface
+    /// </summary>
     public interface INotesManager
     {
+        /// <summary>
+        /// Method declaration to add new note
+        /// </summary>
+        /// <param name="note">note parameter</param>
+        /// <returns>string message</returns>
         public string AddNewNote(NotesModel note);
+
+        /// <summary>
+        /// Method declaration to retrieve all note
+        /// </summary>
+        /// <returns>all notes</returns>
         public IEnumerable<NotesModel> RetrievNote();
+
+        /// <summary>
+        /// Method declaration to remove a note
+        /// </summary>
+        /// <param name="id">note id</param>
+        /// <returns>string message</returns>
         public string RemoveNote(int id);
+
+        /// <summary>
+        /// Method declaration to update a note
+        /// </summary>
+        /// <param name="note">note parameter</param>
+        /// <returns>string message</returns>
         public string UpdateNote(NotesModel note);
     }
 }
