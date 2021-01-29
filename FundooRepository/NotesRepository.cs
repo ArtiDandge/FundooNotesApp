4377326cbd08cf4b4da12b04ed092b31ca676b30
ArtiDandge <58045752+ArtiDandge@users.noreply.github.com>
2021-01-29 09:47:41 +0530

    [Arti Dandge] Refactor: Added StyleCop to make sure that there is no rule voilation in NotesRepository

--- FundooRepository/NotesRepository.cs
+++ FundooRepository/NotesRepository.cs
@@ -1,23 +1,45 @@
-﻿using FundooModels;
-using FundooRepository.Context;
-using FundooRepository.Interfaces;
-using Microsoft.EntityFrameworkCore;
-using System;
-using System.Collections.Generic;
-using System.Linq;
-using System.Text;
+﻿// --------------------------------------------------------------------------------------------------------------------
+// <copyright file="NotesRepository.cs" company="Bridgelabz">
+//   Copyright © 2021 Company="BridgeLabz"
+// </copyright>
+// <creator name="Dandge Arti Subhash"/>
+// --------------------------------------------------------------------------------------------------------------------
 
 namespace FundooRepository
 {
+    using System;
+    using System.Collections.Generic;
+    using System.Linq;
+    using System.Text;
+    using FundooModels;
+    using FundooRepository.Context;
+    using FundooRepository.Interfaces;
+    using Microsoft.EntityFrameworkCore;
+
+    /// <summary>
+    /// NotesRepository class implements INotes Interface
+    /// </summary>
     public class NotesRepository : INotes
     {
+        /// <summary>
+        /// userContext field of type UserContext
+        /// </summary>
         private readonly UserContext userContext;
 
+        /// <summary>
+        /// Initializes a new instance of the <see cref="NotesRepository" /> class.
+        /// </summary>
+        /// <param name="userContext">userContext parameter of type UserContext</param>
         public NotesRepository(UserContext userContext)
         {
             this.userContext = userContext;
         }
 
+        /// <summary>
+        /// Method to add/create new Note to the database
+        /// </summary>
+        /// <param name="note">note parameter of type NotesModel</param>
+        /// <returns>string message</returns>
         public string AddNewNote(NotesModel note)
         {
             this.userContext.FundooNotes.Add(note);
@@ -26,11 +48,15 @@ namespace FundooRepository
             return message;
         }
 
+        /// <summary>
+        /// Method to Retrieve all notes from database
+        /// </summary>
+        /// <returns>all notes from database</returns>
         public IEnumerable<NotesModel> RetrievNote()
         {
             IEnumerable<NotesModel> result;
             IEnumerable<NotesModel> note = this.userContext.FundooNotes;
-            if (note !=null)
+            if (note != null)
             {
                 result = note;
             }
@@ -38,9 +64,15 @@ namespace FundooRepository
             {
                result = null;
             }
+
             return result;   
         }
 
+        /// <summary>
+        /// Method to Remove Note from Database using id
+        /// </summary>
+        /// <param name="id">notes integer id</param>
+        /// <returns>string message</returns>
         public string RemoveNote(int id)
         {
             try
@@ -48,20 +80,26 @@ namespace FundooRepository
                 var note = this.userContext.FundooNotes.Find(id);
                 this.userContext.FundooNotes.Remove(note);
                 this.userContext.SaveChangesAsync();
-                return "Note Deleted Successfully"; ;
+                return "Note Deleted Successfully"; 
             }
-            catch (Exception e)
+            catch
             {
-                throw e;
+                return "Unable to delete this note";
             }
         }
 
+        /// <summary>
+        /// Method to Update Note 
+        /// </summary>
+        /// <param name="note">note parameter of type NotesModel</param>
+        /// <returns>string message</returns>
         public string UpdateNote(NotesModel note)
         {
             if (note.NotesId != 0)
             {
-                userContext.Entry(note).State = EntityState.Modified;
+                this.userContext.Entry(note).State = EntityState.Modified;
             }
+
             this.userContext.SaveChanges();
             string message = "SUCCESS";
             return message;
