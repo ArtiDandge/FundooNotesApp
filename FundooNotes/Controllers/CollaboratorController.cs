4dfe6da851f5c2aa601632c71e443592185309ed
ArtiDandge <58045752+ArtiDandge@users.noreply.github.com>
2021-02-05 12:52:45 +0530

    [Arti Dandge] Refactor: Specified Routes for all api of CollaboratorController and also added stylecop rules

--- FundooNotes/Controllers/CollaboratorController.cs
+++ FundooNotes/Controllers/CollaboratorController.cs
@@ -1,25 +1,50 @@
-﻿using FundooManager.Interfaces;
-using FundooModels;
-using Microsoft.AspNetCore.Mvc;
-using System;
-using System.Collections.Generic;
-using System.Linq;
-using System.Threading.Tasks;
+﻿// --------------------------------------------------------------------------------------------------------------------
+// <copyright file="CollaboratorController.cs" company="Bridgelabz">
+//   Copyright © 2021 Company="BridgeLabz"
+// </copyright>
+// <creator name="Dandge Arti Subhash"/>
+// --------------------------------------------------------------------------------------------------------------------
+
 
 namespace FundooNotes.Controllers
 {
+    using FundooManager.Interfaces;
+    using FundooModels;
+    using Microsoft.AspNetCore.Mvc;
+    using System;
+    using System.Collections.Generic;
+    using System.Linq;
+    using System.Threading.Tasks;
+
+    /// <summary>
+    /// Controller class for api implementation for collaborators
+    /// </summary>
     [ApiController]
     [Route("api/[controller]")]
     public class CollaboratorController : ControllerBase
     {
 
+        /// <summary>
+        /// Field collaboratorManager of type ICollaboratorManager
+        /// </summary>
         private readonly ICollaboratorManager collaboratorManager;
+
+        /// <summary>
+        /// Initializes a new instance of the <see cref="CollaboratorController" /> class.
+        /// </summary>
+        /// <param name="collaboratorManager">collaboratorManager of type ICollaboratorManager</param>
         public CollaboratorController(ICollaboratorManager collaboratorManager)
         {
             this.collaboratorManager = collaboratorManager;
         }
 
+        /// <summary>
+        /// Controller method to add new collaborator
+        /// </summary>
+        /// <param name="collaboraters">collaboraters parameter</param>
+        /// <returns>response data</returns>
         [HttpPost]
+        [Route("AddCollaborator")]
         public IActionResult AddCollaborator(CollaboratorsModel collaboraters)
         {
             try
@@ -38,8 +63,13 @@ namespace FundooNotes.Controllers
             }
         }
 
+        /// <summary>
+        /// Controller method to Remove collaborator
+        /// </summary>
+        /// <param name="id">note id</param>
+        /// <returns>response data</returns>
         [HttpDelete]
-        [Route("{id}")]
+        [Route("RemoveCollaborator/{id}")]
         public IActionResult RemoveCollaborator(int id)
         {
             try
@@ -58,7 +88,12 @@ namespace FundooNotes.Controllers
             }
         }
 
+        /// <summary>
+        /// Controller method to Retrieve All Collaborator
+        /// </summary>
+        /// <returns>response data</returns>
         [HttpGet]
+        [Route("RetrieveAllCollaborator")]
         public IActionResult RetrieveAllCollaborator()
         {
             try
